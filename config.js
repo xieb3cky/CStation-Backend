a3805d113069f999512be4494d7ed56d99139c5e
becky <xieb3cky@gmail.com>
2023-02-28 13:23:53 -0500

    common config for c-station

---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ * Common config for cstation.
+ */
+require("dotenv").config();
+
+const SECRET_KEY = process.env.SECRET_KEY || "secret";
+
+const BCRYPT_WORK_FACTOR = 12;
+
+
+module.exports = {
+    SECRET_KEY,
+    BCRYPT_WORK_FACTOR
+};
