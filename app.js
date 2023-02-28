222440348f0c2d87df1c6110dd8bde017b88b3b2
becky <xieb3cky@gmail.com>
2023-02-28 13:22:39 -0500

    express app for c-station

---
+++ app.js
@@ -0,0 +1,42 @@
+/**Express app for c-station */
+
+require("dotenv").config();
+const express = require('express');
+const cors = require("cors");
+const { NotFoundError } = require('./expressError');
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const userRoutes = require("./routes/user");
+const authRoutes = require("./routes/auth");
+const stationRoutes = require("./routes/station");
+const reviewRoutes = require("./routes/review");
+
+
+
+app.use("/user", userRoutes);
+app.use("/auth", authRoutes);
+app.use("/station", stationRoutes);
+app.use("/review", reviewRoutes);
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+    return next(new NotFoundError());
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: { message, status },
+    });
+});
+
+module.exports = app;
