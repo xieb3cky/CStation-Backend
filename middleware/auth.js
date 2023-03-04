5c94e6504f1e98ac2fa2648982b48df42edc2b7d
becky <xieb3cky@gmail.com>
2023-03-04 15:12:29 -0500

    added middleware to ensure correct user & logged in user

--- middleware/auth.js
+++ middleware/auth.js
@@ -28,6 +28,24 @@ function authenticateJWT(req, res, next) {
   }
 }
 
+/** Middleware to use when they must provide a valid token & be user matching
+ *  username provided as route param.
+ *
+ *  If not, raises Unauthorized.
+ */
+
+function ensureCorrectUser(req, res, next) {
+  try {
+    const user = res.locals.user;
+    if (!(user && (user.username === req.params.username))) {
+      throw new UnauthorizedError();
+    }
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
 /** Middleware to use when they must be logged in.
  *
  * If not, raises Unauthorized.
@@ -44,8 +62,8 @@ function ensureLoggedIn(req, res, next) {
 
 
 
-
 module.exports = {
   authenticateJWT,
+  ensureCorrectUser,
   ensureLoggedIn
 };
