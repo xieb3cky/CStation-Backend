d653668e50fde4bb1181969988e3293000392449
becky <xieb3cky@gmail.com>
2023-04-18 13:59:52 -0400

    update env variables

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
 /** Database setup */
 
 const { Client } = require("pg");
-
 let DB_URI;
 
 if (process.env.NODE_ENV === "test") {
     DB_URI = "postgresql:///cstation_test";
 } else {
-    DB_URI = "postgresql:///cstation";
+    DB_URI = process.env.DATABASE_URL;
 }
 
 let db = new Client({
