4b25c52936f83c673e681fe98eeecbed5b43f371
becky <xieb3cky@gmail.com>
2023-04-25 15:19:00 -0400

    d

--- db.js
+++ db.js
@@ -29,13 +29,13 @@ const db = new Client({
 
 db.connect();
 
-db.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-        console.log(JSON.stringify(row));
-    }
-    db.end();
-});
+// db.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//     if (err) throw err;
+//     for (let row of res.rows) {
+//         console.log(JSON.stringify(row));
+//     }
+//     db.end();
+// });
 
 
 module.exports = db;
